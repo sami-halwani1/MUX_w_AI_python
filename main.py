7533c435bc7790a7905827d4cc2e3732cbc21aa0
sami-halwani1 <sami.halwani1@gmail.com>
2025-02-03 13:32:36 -0800

    InitialWork

---
+++ main.py
@@ -0,0 +1,32 @@
+import matplotlib
+import numpy
+import json
+import ai_scaler
+
+
+event = [{
+    "isoTable": {
+        "USD": 1.00,
+        "EUR": .908,
+        "GBP": .775,
+        "CAD": 1.33,
+        "CHF": .997,
+        "JPY": 109.032,
+        "AUD": 1.476,
+        "HKD": 7.820,
+        "NZD": 1.550,
+        "SEK": 9.61,
+        "KRW": 1172.420,
+        "SGD": 1.365,
+        "NOK": 9.180,
+        "MXN": 19.440,
+        "MUX": 1.002
+    }
+}]
+
+def __init__():
+
+    
+    
+    return 
+
